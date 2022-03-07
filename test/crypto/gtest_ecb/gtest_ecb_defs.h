1235317a13ce2b6f8818dbfc41b0ace72f467500
takoslab <takotakotakos@outlook.jp>
2022-03-07 22:28:17 +0900

    Add

---
+++ test/crypto/gtest_ecb/gtest_ecb_defs.h
@@ -0,0 +1,28 @@
+/*!
+* cryptography library
+*
+* Copyright (c) 2022 tako
+*
+* This software is released under the MIT license.
+* see https://opensource.org/licenses/MIT
+*/
+
+#ifndef GTEST_ECB_DEFS_H
+#define GTEST_ECB_DEFS_H
+
+#include <stdint.h>
+
+const char ECB_TEST_STRING[80] = {
+  '1', '2', '3', '4', '5', '6', '7', '8',
+  '1', '2', '3', '4', '5', '6', '7', '8',
+  '1', '2', '3', '4', '5', '6', '7', '8',
+  '1', '2', '3', '4', '5', '6', '7', '8',
+  '1', '2', '3', '4', '5', '6', '7', '8',
+  '1', '2', '3', '4', '5', '6', '7', '8',
+  '1', '2', '3', '4', '5', '6', '7', '8',
+  '1', '2', '3', '4', '5', '6', '7', '8',
+  '1', '2', '3', '4', '5', '6', '7', '8',
+  '1', '2', '3', '4', '5', '6', '7', '8',
+};
+
+#endif
