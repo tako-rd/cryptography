d637945ce2875b7ff41892f0408f39744fd79302
takoslab <takotakotakos@outlook.jp>
2022-04-02 06:46:12 +0900

    Add gtest_camellia.
    Add the gtest module for camellia.

---
+++ test/crypto/gtest_camellia/gtest_camellia128.cc
@@ -0,0 +1,67 @@
+/*!
+* cryptography library
+*
+* Copyright (c) 2022 tako
+*
+* This software is released under the MIT license.
+* see https://opensource.org/licenses/MIT
+*/
+
+#include "gtest_camellia128.h"
+
+TEST_F(GTestCamellia128, Normal_encrypt_to_decrypt_001) {
+  cryptography::camellia camellia;
+  uint8_t ciphertext[16] = {0};
+  uint8_t plaintext[16] = {0};
+
+  camellia.initialize(cryptography::CAMELLIA128, CAMELLIA_EXAM_128BIT_KEY, sizeof(CAMELLIA_EXAM_128BIT_KEY), false);
+
+  camellia.encrypt(CAMELLIA_EXAM_PLAINTEXT, sizeof(CAMELLIA_EXAM_PLAINTEXT), ciphertext, sizeof(ciphertext));
+  for (uint64_t i = 0; i < 16; ++i) {
+    EXPECT_EQ(CAMELLIA_EXAM_128BIT_CIPHERTEXT[i], ciphertext[i]);
+  }
+
+  camellia.decrypt(ciphertext, sizeof(ciphertext), plaintext, sizeof(plaintext));
+  for (uint64_t i = 0; i < 16; ++i) {
+    EXPECT_EQ(CAMELLIA_EXAM_PLAINTEXT[i], plaintext[i]);
+  }
+
+}
+
+TEST_F(GTestCamellia128, Normal_encrypt_to_decrypt_002) {
+  cryptography::camellia camellia;
+  uint8_t ciphertext[16] = {0};
+  uint8_t plaintext[16] = {0};
+
+  camellia.initialize(cryptography::CAMELLIA192, CAMELLIA_EXAM_192BIT_KEY, sizeof(CAMELLIA_EXAM_192BIT_KEY), false);
+
+  camellia.encrypt(CAMELLIA_EXAM_PLAINTEXT, sizeof(CAMELLIA_EXAM_PLAINTEXT), ciphertext, sizeof(ciphertext));
+  for (uint64_t i = 0; i < 16; ++i) {
+    EXPECT_EQ(CAMELLIA_EXAM_192BIT_CIPHERTEXT[i], ciphertext[i]);
+  }
+
+  camellia.decrypt(ciphertext, sizeof(ciphertext), plaintext, sizeof(plaintext));
+  for (uint64_t i = 0; i < 16; ++i) {
+    EXPECT_EQ(CAMELLIA_EXAM_PLAINTEXT[i], plaintext[i]);
+  }
+
+}
+
+TEST_F(GTestCamellia128, Normal_encrypt_to_decrypt_003) {
+  cryptography::camellia camellia;
+  uint8_t ciphertext[16] = {0};
+  uint8_t plaintext[16] = {0};
+
+  camellia.initialize(cryptography::CAMELLIA256, CAMELLIA_EXAM_256BIT_KEY, sizeof(CAMELLIA_EXAM_256BIT_KEY), false);
+
+  camellia.encrypt(CAMELLIA_EXAM_PLAINTEXT, sizeof(CAMELLIA_EXAM_PLAINTEXT), ciphertext, sizeof(ciphertext));
+  for (uint64_t i = 0; i < 16; ++i) {
+    EXPECT_EQ(CAMELLIA_EXAM_256BIT_CIPHERTEXT[i], ciphertext[i]);
+  }
+
+  camellia.decrypt(ciphertext, sizeof(ciphertext), plaintext, sizeof(plaintext));
+  for (uint64_t i = 0; i < 16; ++i) {
+    EXPECT_EQ(CAMELLIA_EXAM_PLAINTEXT[i], plaintext[i]);
+  }
+
+}
