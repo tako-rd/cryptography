9d521d76748f5b62d3d76bfa57ee1f2bcdec494d
takoslab <takotakotakos@outlook.jp>
2022-04-12 21:51:47 +0900

    Add CAST128 module.

---
+++ src/include/crypto/cast128.h
@@ -0,0 +1,59 @@
+/*!
+* cryptography library
+*
+* Copyright (c) 2022 tako
+*
+* This software is released under the MIT license.
+* see https://opensource.org/licenses/MIT
+*/
+
+#include "defs.h"
+#include "block_cipher.h"
+
+#ifndef CAST128_H
+#define CAST128_H
+
+namespace cryptography {
+
+class cast128 final : public algorithm<cast128> {
+ public:
+  cast128() {};
+
+  ~cast128() {};
+
+  int32_t initialize(const uint32_t mode, const uint8_t *key, const uint32_t ksize, bool enable_intrinsic) noexcept;
+
+  int32_t encrypt(const uint8_t * const ptext, const uint32_t psize, uint8_t *ctext, const uint32_t csize) noexcept;
+
+  int32_t decrypt(const uint8_t * const ctext, const uint32_t csize, uint8_t *ptext, const uint32_t psize) noexcept;
+
+  void clear() noexcept;
+
+ private:
+  void no_intrinsic_encrypt(const uint8_t * const ptext, uint8_t *ctext) const noexcept;
+
+  void no_intrinsic_decrypt(const uint8_t * const ctext, uint8_t *ptext) const noexcept;
+
+  void intrinsic_encrypt(const uint8_t * const ptext, uint8_t *ctext) const noexcept;
+
+  void intrinsic_decrypt(const uint8_t * const ctext, uint8_t *ptext) const noexcept;
+
+  void expand_key(const uint32_t * const key, uint32_t *km, uint32_t *kr) noexcept;
+
+  uint32_t f_function(uint32_t d, uint32_t kmi, uint32_t kri, uint32_t round) const noexcept;
+
+  uint32_t km_[16];
+
+  uint32_t kr_[16];
+
+  uint32_t mode_;
+
+  bool has_subkeys_;
+
+  bool enable_intrinsic_func_;
+};
+
+}
+
+#endif
+
