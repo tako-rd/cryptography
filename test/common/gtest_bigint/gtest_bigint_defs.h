31d06585a49115b04b2e57b55248469dfb73d4b0
takoslab <takotakotakos@outlook.jp>
2022-05-04 02:56:18 +0900

    Add bigint gtest module.
    Forgot to add.

---
+++ test/common/gtest_bigint/gtest_bigint_defs.h
@@ -0,0 +1,15 @@
+/*!
+ * cryptography library
+ *
+ * Copyright (c) 2022 tako
+ *
+ * This software is released under the MIT license.
+ * see https://opensource.org/licenses/MIT
+ */
+
+#ifndef GTEST_BIGINT_DEFS_H
+#define GTEST_BIGINT_DEFS_H
+
+#include <stdint.h>
+
+#endif
