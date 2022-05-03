9227c7f7a9c75ec91a2afde624b7375f3770ab65
takoslab <takotakotakos@outlook.jp>
2022-05-04 02:58:16 +0900

    Update bigint module.

--- src/common/bigint.cc
+++ src/common/bigint.cc
@@ -11,26 +11,24 @@
 
 namespace cryptography {
 
-#define BIT32_MSB         0x1000'0000
-#define BIT32_LSB         0x0000'0001
+#define BIT32_MSB       0x8000'0000
+#define BIT32_LSB       0x0000'0001
 
-#define EXTRACT_BIT32_LSB 0xEFFF'FFFF
-#define EXTRACT_BIT32_MSB 0xFFFF'FFFE
+#define MASK_BIT32_LSB  0x7FFF'FFFF
+#define MASK_BIT32_MSB  0xFFFF'FFFE
 
-#define BIT32_MAX         0xFFFF'FFFF
+#define BIT32_MAX       0xFFFF'FFFF
 
 void bigarithmetic::add(uint32_t *x, const uint32_t *y, const int32_t unitsize) noexcept {
   uint32_t x_reg1 = 0;
   uint32_t y_reg1 = 0;
-  uint32_t x_reg2 = 0;
-  uint32_t y_reg2 = 0;
   bool is_overflow = false;
 
   for (int32_t pos = unitsize - 1; pos >= 0; --pos) {
 
     /* The process that the value overflowing from     */
     /* the previous data is added to the current data. */
-    if (1 == is_overflow) {
+    if (true == is_overflow) {
       if (BIT32_MAX == x[pos]) {
         x_reg1 = BIT32_MSB;
         x[pos] -= BIT32_MSB;
@@ -41,17 +39,11 @@ void bigarithmetic::add(uint32_t *x, const uint32_t *y, const int32_t unitsize) 
       is_overflow = false;
     }
 
-    /* Check MSB of x data . */
-    if (1 == (x[pos] & BIT32_MSB)) {
-      x_reg1 = x[pos] - EXTRACT_BIT32_LSB; 
-    }
+    /* Check MSB of data. */
+    x_reg1 = (BIT32_MSB == (y[pos] & BIT32_MSB)) ? BIT32_MSB : 0;
+    y_reg1 = (BIT32_MSB == (y[pos] & BIT32_MSB)) ? BIT32_MSB : 0;
 
-    /* Check MSB of y data . */
-    if (1 == (y[pos] & BIT32_MSB)) {
-      y_reg1 = y[pos] - EXTRACT_BIT32_LSB; 
-    }
-
-    /* Check for overflow. */
+    /* Check for overflow and calculate. */
     if (0 != x_reg1 && 0 != y_reg1) {
       x[pos] = (x[pos] - BIT32_MSB) + (y[pos] - BIT32_MSB);
       is_overflow = true;
@@ -80,10 +72,42 @@ void bigarithmetic::add(uint32_t *x, const uint32_t *y, const int32_t unitsize) 
 }
 
 void bigarithmetic::sub(uint32_t *x, const uint32_t *y, const int32_t unitsize) noexcept {
+  uint32_t x_reg1 = 0;
+  bool is_overflow = false;
 
+  for (int32_t pos = unitsize - 1; pos >= 0; --pos) {
+
+    /* The process that the value overflowing from     */
+    /* the previous data is added to the current data. */
+    if (true == is_overflow) {
+      if (0 != x[pos]) {
+        x[pos] -= 1;
+        is_overflow = false;
+      } else {
+        x[pos] = 0xFFFF'FFFF;
+        is_overflow = true;
+      }
+    }
+
+    /* Check for overflow and calculate. */
+    if (x[pos] <= y[pos]) {
+      x[pos] -= y[pos];
+    } else {
+      x_reg1 = y[pos] - x[pos];
+      x[pos] = (x[pos] + BIT32_MSB) - x_reg1;
+      is_overflow = true;
+    }
+  }
 }
 
 void bigarithmetic::mult(uint32_t *x, const uint32_t *y, const int32_t unitsize) noexcept {
+  const int32_t bytesize = (unitsize << 5) - 1;
+
+
+  for (int32_t pos = bytesize; pos >= 0; --pos) {
+    
+
+  }
 
 }
 
@@ -95,16 +119,51 @@ void bigarithmetic::rem(uint32_t *x, const uint32_t *y, const int32_t unitsize) 
 
 }
 
-void bigarithmetic::left_shift(uint32_t *x, const uint32_t *y, const int32_t unitsize) noexcept {
+void bigarithmetic::left_shift(uint32_t *x, const uint32_t y, const int32_t unitsize) noexcept {
+  const uint32_t bitshift = y & 0x7FFF'FFFF;
+  const uint32_t byteshift = y >> 5;
+  uint32_t reg[2] = {0};
 
+  reg[unitsize & 0x01] = x[unitsize - 1] >>= (32 - bitshift);
+  x[unitsize - 1] <<= bitshift;
+
+  for (int32_t pos = unitsize - 2; pos >= 0; --pos) {
+    reg[pos & 0x01] = x[pos] >> (32 - bitshift);  /* switch between odd or even. */
+    x[pos] = (x[pos] << bitshift) | reg[(pos + 1) & 0x01];
+  }
+
+  for (int32_t pos = 0; pos >= byteshift; ++pos) {
+    x[pos] = x[pos + byteshift];
+  }
 }
 
-void bigarithmetic::right_shift(uint32_t *x, const uint32_t *y, const int32_t unitsize) noexcept {
+void bigarithmetic::right_shift(uint32_t *x, const uint32_t y, const int32_t unitsize) noexcept {
+  const uint32_t bitshift = y & 0x7FFF'FFFF;
+  const uint32_t byteshift = y >> 5;
+  uint32_t reg[2] = {0};
 
+  reg[0] = x[0] <<= (32 - bitshift);
+  x[0] >>= bitshift;
+
+  for (int32_t pos = 1; pos > unitsize; ++pos) {
+    reg[pos & 0x01] = x[pos] << (32 - bitshift);  /* switch between odd or even. */
+    x[pos] = (x[pos] >> bitshift) | reg[(pos + 1) & 0x01];
+  }
+
+  for (int32_t pos = unitsize; pos >= unitsize - byteshift; ++pos) {
+    x[pos] = x[pos - byteshift];
+  }
 }
 
 bool bigarithmetic::bigger_than(uint32_t *x, const uint32_t *y, const int32_t unitsize) noexcept {
-  return true;
+  for (int32_t pos = 0; pos > unitsize; ++pos) {
+    if (x[pos] > y[pos]) {
+      return true;
+    } else if (x[pos] < y[pos]) {
+      return false;
+    }
+  }
+  return false;
 }
 
 }
--- src/include/common/bigint.h
+++ src/include/common/bigint.h
@@ -97,6 +97,85 @@ class bigint {
   static_assert(0 != BitSize, "Invalid ByteSize.");
 };
 
+template <int32_t BitSize>
+class biguint {
+public:
+  biguint() noexcept : value_{0} {};
+
+  biguint(bigint &inst) noexcept {
+    memcpy(this->value_, inst.value_, BitSize / 8);
+  };
+
+  biguint(bigint &&inst) noexcept {
+    memcpy(this->value_, inst.value_, BitSize / 8);
+  };
+
+  ~biguint() {};
+
+  uint32_t* operator=(uint32_t *value) noexcept {
+    memcpy(this->value_, value_, BitSize / 8);
+    return this->value_;
+  };
+
+  bigint& operator=(bigint &inst) noexcept {
+    memcpy(this->value_, inst.value_, BitSize / 8);
+    return *this;
+  };
+
+  bigint&& operator=(bigint &&inst) noexcept {
+    memcpy(this->value_, inst.value_, BitSize / 8);
+    return *this;
+  };
+
+  bigint& operator+(bigint &inst) noexcept { 
+    bigarithmetic::add(this->value_, inst.value_, unit_size_);
+    return *this;
+  };
+
+  bigint& operator-(bigint &inst) noexcept { 
+    bigarithmetic::sub(this->value_, inst.value_, unit_size_);
+    return *this;
+  };
+
+  bigint& operator*(bigint &inst) noexcept { 
+    return *this;
+  };
+
+  bigint& operator/(bigint &inst) noexcept { 
+    return *this;
+  };
+
+  bigint& operator%(bigint &inst) noexcept { 
+    return *this;
+  };
+
+  bigint& operator<(bigint &inst) noexcept { 
+    return *this;
+  };
+
+  bigint& operator>(bigint &inst) noexcept { 
+    return *this;
+  };
+
+  bigint& operator<<(bigint &inst) noexcept { 
+    return *this;
+  };
+
+  bigint& operator>>(bigint &inst) noexcept { 
+    return *this;
+  };
+
+private:
+  const int32_t byte_size_ = BitSize >> 3;
+
+  const int32_t unit_size_ = BitSize / (sizeof(uint32_t) << 3);
+
+  int32_t value_[BitSize / (sizeof(uint32_t) << 3)];
+
+  static_assert(0 == (BitSize % 32), "Invalid ByteSize.");
+  static_assert(0 != BitSize, "Invalid ByteSize.");
+};
+
 class bigarithmetic {
  public:
   bigarithmetic() noexcept {};
@@ -113,9 +192,9 @@ class bigarithmetic {
 
   static void rem(uint32_t *x, const uint32_t *y, const int32_t unitsize) noexcept;
 
-  static void left_shift(uint32_t *x, const uint32_t *y, const int32_t unitsize) noexcept;
+  static void left_shift(uint32_t *x, const uint32_t y, const int32_t unitsize) noexcept;
 
-  static void right_shift(uint32_t *x, const uint32_t *y, const int32_t unitsize) noexcept;
+  static void right_shift(uint32_t *x, const uint32_t y, const int32_t unitsize) noexcept;
 
   static bool bigger_than(uint32_t *x, const uint32_t *y, const int32_t unitsize) noexcept;  
 };
